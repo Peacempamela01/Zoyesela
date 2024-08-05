378ada6f88c4f04d39e0aafd2b09a575b195c6f0
peace mpamela <peampamela@gmail.com>
2024-08-05 16:36:00 +0200

    my work

---
+++ peace.js
@@ -0,0 +1,29 @@
+
+//1  x = 5;
+// y = 10;
+// z = 15;
+// result = (x + y)*z
+// console.log(result);
+
+// 2. var hie = "HELLO";
+//  var Fullname = "PEACEMPAMELA";
+ 
+//  result = hie+Fullname 
+//  console.log(result);
+
+// 3 r = 4;
+// pie = 22/7;
+// area = r * pie^2;
+// result = area
+// console.log(area);
+
+let num1 = 5;
+let num2 = 10;
+
+num1 = num1 + num2;
+num2 =  num1 - num2;
+num1 = num1 - num2;
+
+console.log("after swapping using arithmetic operation");
+console.log("num1 =",num1);
+console.log("num2 =", num2);
